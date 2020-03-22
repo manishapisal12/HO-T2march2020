5379d188d89d295089b66dd0d73134fef8fc693a
manisha pisal <manupisal12@gmail.com>
2020-03-22 20:43:56 +0530

    adding the second commit

---
+++ start5.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class satr5
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=4;i++)
+{
+for(int j=1;i<=i;i++)
+{
+System.out.print(" ");
+}
+for(int j=4;j>=i;j--)
+{
+System.out.print("*");
+}
+System.out.prinln():
+}
+}
+}
